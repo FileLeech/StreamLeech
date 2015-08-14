5d66000123714f44cbd5dc36f40c7f10d87c7cae
federalelk0 <federalelk0@xlFederalElk0lx>
2015-08-13 20:09:37 -0500

    Configuracion del Player

--- pages/header.php
+++ pages/header.php
@@ -7,15 +7,27 @@
         <script type="text/javascript" src="/js/jquery.min.js"></script>
         <script type="text/javascript" src="/player/jwplayer.js"></script>
         <script type="text/javascript">jwplayer.key = "Wz0ISkuJjUlK9YCu3UkmhMLQYR4WpTl75P5iQ4U78t2rb1NQmNWBlQ==";</script>
+        <link href="/player/stormtrooper.css" rel="stylesheet" type="text/css" />
         <style>
             body{
                 color: #333333;
                 padding: 0px;
                 margin: 0px;
+                text-align: center;
+            }
+            .StreamLeech{
+                position: relative;
+            }
+            .publicidad{
+                position: absolute;
+                top: 0;
+                left: 0;
                 width: 100%;
-                max-width: 100%;
                 height: 100%;
-                max-height: 100%;
+                border-radius: 4px;
+                background: #333333;
+                opacity: 0.4;
+                margin-left: auto;
             }
         </style>
     </head>
@@ -36,25 +48,37 @@
             });
 
             function putPlayer() {
-                var LeechPlayer = jwplayer("StreamLeech");
-                if (isRTMP > 0) {
+                try {
+                    var LeechPlayer = jwplayer("StreamLeech");
+                    if (isRTMP > 0) {
 
-                } else {
-                    LeechPlayer.setup({
-                        controls: true,
-                        width: "100%",
-                        height: "100%",
-                        aspectratio: "16:9",
-                        fullscreen: "true",
-                        primary: 'html5',
-                        provider: 'http',
-                        autostart: false,
-                        sources: sources_player,
-                        image: img_player,
-                        abouttext: "StreamLeech 1.0",
-                        aboutlink: "http://www.streamleech.ga"
+                    } else {
+                        LeechPlayer.setup({
+                            controls: true,
+                            fullscreen: "true",
+                            primary: 'html5',
+                            provider: 'http',
+                            width: 600,
+                            height: 400,
+                            aspectratio: " 4:3",
+                            autostart: false,
+                            sources: sources_player,
+                            image: img_player,
+                            abouttext: "StreamLeech 1.0",
+                            aboutlink: "http://www.streamleech.ga",
+                            skin: {
+                                name: "stormtrooper"
+                            }
+                        });
+                    }
+                    LeechPlayer.on("ready", function () {
+                        if (LeechPlayer.getRenderingMode() == 'html5') {
+                          $("#StreamLeech").append('<div class="publicidad">mexico</div>');
+                        }
                     });
+                } catch (e) {
+                    alert(e);
                 }
             }
         </script>
-        <div id="StreamLeech"></div>
+    <center><div id="StreamLeech" class="StreamLeech"></div></center>
